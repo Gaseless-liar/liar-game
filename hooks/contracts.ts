900393f7605634c3496f5cc850d2e97196f00d36
Iris <iris.devillars@gmail.com>
2022-10-30 00:39:06 +0100

    update contracts addr goerli2

--- hooks/contracts.ts
+++ hooks/contracts.ts
@@ -3,8 +3,7 @@ import erc20_abi from "../abi/erc20_abi.json";
 import { Abi, Contract } from "starknet";
 
 export const gaslessLiarContract: string =
-    "0x03318a1deb68f534113f421ec5a7a05ca9e0f5c3803bae195b46d8af630834d1"
-//   "0x01d3c0f481a1adacdf251d9af2710ea119e0facde4dfd8b6269bb8ef7c6889e5";
+  "0x01d3c0f481a1adacdf251d9af2710ea119e0facde4dfd8b6269bb8ef7c6889e5";
 
 export const ethereumContract: string =
   "0x049D36570D4e46f48e99674bd3fcc84644DdD6b96F7C741B1562B82f9e004dC7";
